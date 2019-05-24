25a219e56a3b12f24c924ad7c2cdb5e8bfdb8b73
Brayden Chumbley <bchumbley2001@gmail.com>
2019-05-24 12:45:26 -0400

    Added Utils class. Moved drawCenteredString into Utils class

---
+++ Battleship/src/battleship/game/Utils.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package battleship.game;
+
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+/**
+ *
+ * @author Brayden Chumbley
+ */
+public class Utils {
+    
+    public static void drawCenteredString(Graphics g, String text, Rectangle rect, Font font) {
+	// Get the FontMetrics
+	FontMetrics metrics = g.getFontMetrics(font);
+	// Determine the X coordinate for the text
+	int x = rect.x + (rect.width - metrics.stringWidth(text)) / 2;
+	// Determine the Y coordinate for the text (note we add the ascent, as in java 2d 0 is top of the screen)
+	int y = rect.y + ((rect.height - metrics.getHeight()) / 2) + metrics.getAscent();
+	// Set the font
+	g.setFont(font);
+	// Draw the String
+	g.drawString(text, x, y);
+    }
+    
+}
