082a0176ac58b540aa143a57aebb390e0c0537d4
Brayden Chumbley <bchumbley2001@gmail.com>
2019-05-25 14:22:14 -0400

    Add 4 menu buttons with basic action events

--- Battleship/src/battleship/game/MainMenu.java
+++ Battleship/src/battleship/game/MainMenu.java
@@ -5,13 +5,17 @@
  */
 package battleship.game;
 
+import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.image.BufferedImage;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 /**
@@ -20,15 +24,85 @@ import javax.swing.JPanel;
  */
 public class MainMenu extends JPanel {
     
-    private BufferedImage background;
+    private Image background;
 
     public MainMenu() {
         try {
-            background = ImageIO.read(new File("res\\gfx\\menubackground.png"));
-            setBounds(0,0,Game.WIDTH,Game.HEIGHT);
+            init();
         } catch (IOException ex) {
             Logger.getLogger(MainMenu.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    private void init() throws IOException{
+	setLayout(null);
+	background = ImageIO.read(new File("res\\gfx\\menubackground.png")).getScaledInstance(Game.WIDTH, Game.HEIGHT, Image.SCALE_FAST);
+	setBounds(0, 0, Game.WIDTH, Game.HEIGHT);
+	
+	JPanel btnPanel = new JPanel();
+	btnPanel.setLayout(null);
+	btnPanel.setBounds(100, 0, 300, Game.HEIGHT);
+	btnPanel.setBackground(new Color(50, 50, 50, 200));
+	addButtons(btnPanel);
+	add(btnPanel);
+	
+    }
+    
+    private void addButtons(JPanel p){
+	
+	JButton play, options, credits, quit;
+	int padding = 10;
+	
+	//Play Button
+	
+	play = new Button("Play");
+	play.setBounds(padding, p.getHeight()/4, p.getWidth()-padding*2, 40);
+	play.addActionListener(new ActionListener(){
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		System.out.println("Play");
+	    }
+	});
+	
+	
+	//Options Button
+	
+	options = new Button("Options");
+	options.setBounds(padding, p.getHeight()/4 + 60, p.getWidth()-padding*2, 40);
+	options.addActionListener(new ActionListener(){
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		System.out.println("Options");
+	    }
+	});
+	
+	//Credits Button
+	
+	credits = new Button("Credits");
+	credits.setBounds(padding, p.getHeight()/4 + 120, p.getWidth()-padding*2, 40);
+	credits.addActionListener(new ActionListener(){
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		System.out.println("Credits");
+	    }
+	});
+	
+	//Quit Button
+	
+	quit = new Button("Quit");
+	quit.setBounds(padding, p.getHeight()/4 + 180, p.getWidth()-padding*2, 40);
+	quit.addActionListener(new ActionListener(){
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		System.exit(0);
+	    }
+	});
+	
+	p.add(play);
+	p.add(options);
+	p.add(credits);
+	p.add(quit);
+	
     }
     
     @Override
