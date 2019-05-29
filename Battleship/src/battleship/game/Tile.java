51a711fbe8f2d9e1a50a49c65d89c18b6dd37dd8
toyu2550 <toyu2550@8R71Q52.Student.UGDSB.ED>
2019-05-29 14:01:45 -0400

    added tiles

---
+++ Battleship/src/battleship/game/Tile.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleship.game;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+
+public class Tile extends AbstractGameObject{
+
+    public Tile(double x,double y){
+        this.x=x;
+        this.y=y;
+    }
+    
+    
+    @Override
+    public void draw(Graphics g){
+        g.setColor(Color.red);
+        g.drawRect((int)x,(int)y,50,50);
+        //g.drawImage(sprite, (int)x, (int)y, null)
+    }
+}
