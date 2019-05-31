d9b92d3721972f8fc09a4ee835cfc9adb60e0d8b
Brayden Chumbley <bchumbley2001@gmail.com>
2019-05-31 13:36:53 -0400

    reformatted Tile.java

--- Battleship/src/battleship/game/Tile.java
+++ Battleship/src/battleship/game/Tile.java
@@ -4,35 +4,39 @@
  * This class draws tiles
  */
 package battleship.game;
+
 import java.awt.Color;
 import java.awt.Graphics;
 
-
-public class Tile extends AbstractGameObject{
+public class Tile extends AbstractGameObject {
+    
+    public static final int TILE_SIZE = 50;
 
     /**
      * This constructor i used to set the coordinates of the tiles
-     * @param x - The location of the tile on the x-axis 
+     *
+     * @param x - The location of the tile on the x-axis
      * @param y - The location of the tile on the y-axis
      */
-    public Tile(int x, int y){
-        pos = new Coordinate(x, y);
+    public Tile(int x, int y) {
+	pos = new Coordinate(x, y);
     }
-    
+
     @Override
     public void update() {
-	
+
     }
-    
+
     /**
      * Used to draw the tiles
+     *
      * @param g - Graphics object that is being drawn
      */
     @Override
-    public void draw(Graphics g){
-        g.setColor(Color.red);
-        g.drawRect(pos.getX(), pos.getY(),50,50);
-        //g.drawImage(sprite, (int)x, (int)y, null)
+    public void draw(Graphics g) {
+	g.setColor(Color.red);
+	g.drawRect(pos.getX(), pos.getY(), TILE_SIZE, TILE_SIZE);
+	//g.drawImage(sprite, (int)x, (int)y, null)
     }
 
     @Override
