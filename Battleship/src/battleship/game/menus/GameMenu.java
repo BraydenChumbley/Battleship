dcb7f9a1a35da30a78570ba53e92f5f2694667b1
Brayden Chumbley <bchumbley2001@gmail.com>
2019-06-06 10:06:29 -0400

    Added leave btn to game panel

--- Battleship/src/battleship/game/menus/GameMenu.java
+++ Battleship/src/battleship/game/menus/GameMenu.java
@@ -5,9 +5,14 @@
  */
 package battleship.game.menus;
 
+import battleship.game.GAME_STATE;
 import battleship.game.Game;
-import battleship.game.Tile;
+import battleship.game.uicomponents.Button;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseListener;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 /**
@@ -23,6 +28,30 @@ public class GameMenu extends JPanel {
     private void init() {
 	setBackground(Color.DARK_GRAY);
 	setBounds(Game.WIDTH - 300, 0, 300, Game.HEIGHT);
+	
+	addButtons(this);
+    }
+    
+    private void addButtons(JPanel p){
+	
+	JButton leaveBtn, undoBtn, readyBtn;
+	
+	int xMargin = 25, yMargin = 50, btnHeight = 50;
+	
+	leaveBtn = new Button("Leave", xMargin, p.getHeight() - (btnHeight + yMargin), p.getWidth() - (xMargin*2), btnHeight);
+	leaveBtn.addActionListener(new ActionListener(){
+	    
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		Game.setGameState(GAME_STATE.MAIN_MENU);
+	    }
+	
+	});
+	
+	leaveBtn.addMouseListener((MouseListener) leaveBtn);
+	
+	p.add(leaveBtn);
+	
     }
 
 }
